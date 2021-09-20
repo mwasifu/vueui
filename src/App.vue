0a64246fb3bc80d6bacede7f4d4059d8356cdbaa
mwasifu <56853245+mwasifu@users.noreply.github.com>
2021-09-20 14:49:36 -0500

    reorganized
    navigation doesnt work yet

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,39 @@
     <v-content>
       <AppBar />
 
+       <v-container class="heading-container">
+        <v-row class="text-center">
+          <v-col class="mb-4 headingsep">
+            <span class="display-2 font-weight-light mb-3 heading blog"
+              >Blog</span
+            >
+            <span class="display-2 font-weight-medium mb-3 heading meister"
+              >meister</span
+            >
+
+            <p class="subheading font-weight-regular firstParagraph">
+              Still in the oven!
+              <a
+                class="discord"
+                href="https://discord.gg/QFhG6jnu"
+                target="_blank"
+              >
+                Visit us on Discord!</a
+              >
+            </p>
+          </v-col>
+        </v-row>
+      </v-container>
+
+
+
+
       <NewPost />
     </v-content>
 
     <v-card height="800px">
       <v-footer v-bind="localAttrs" :padless="padless" fixed color="black">
-        <v-card-text class="white--text">
+        <v-card-text class="white--text footer-info">
           {{ new Date().getFullYear() }} — <em>Blogmeister Inc</em>
         </v-card-text>
       </v-footer>
@@ -17,7 +44,6 @@
 </template>
 
 
-// javascript
 
 
 <script>
@@ -36,7 +62,6 @@ export default {
 </script>
 
 
-// css
 
 
 <style scoped>
@@ -46,5 +71,24 @@ export default {
   font-family: Arial, Helvetica, sans-serif;
   padding-left: 2%;
 }
+
+.footer-info{
+  text-align: center;
+}
+
+.heading-container{
+  justify-content: center;
+  position: relative;
+  padding-top: 15%;
+}
+
+.discord {
+  color: orange;
+}
+
+.blog {
+  color: orange;
+}
+
 
 </style>
--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,52 +1,7 @@
 <template>
   <div>
-    <v-app-bar
-      :collapse="!collapseOnScroll"
-      :collapse-on-scroll="collapseOnScroll"
-      fixed
-      color="charcoal"
-      dark
-      scroll-target="#scrolling-techniques-6"
-    >
-      <!-- <v-card class="overflow-hidden">
-        <v-sheet
-          height="800"
-          class="overflow-hidden"
-          style="position: relative"
-        >
-          <v-container class="fill-height"> -->
-            <v-btn @click="drawer != drawer">+</v-btn>
-
-            <v-navigation-drawer app v-model="drawer" absolute temporary>
-              <v-list-item>
-                <v-list-item-avatar>
-                  <v-img
-                    src="https://randomuser.me/api/portraits/men/78.jpg"
-                  ></v-img>
-                </v-list-item-avatar>
-
-                <v-list-item-content>
-                  <v-list-item-title>John Leider</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-
-              <v-divider></v-divider>
-
-              <v-list dense>
-                <v-list-item v-for="item in items" :key="item.title" link>
-                  <v-list-item-icon>
-                    <v-icon>{{ item.icon }}</v-icon>
-                  </v-list-item-icon>
-
-                  <v-list-item-content>
-                    <v-list-item-title>{{ item.title }}</v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list>
-            </v-navigation-drawer>
-          <!-- </v-container>
-        </v-sheet>
-      </v-card> -->
+    <v-app-bar fixed color="charcoal" dark>
+      <v-btn flat @click="drawer != drawer">+</v-btn>
 
       <v-spacer></v-spacer>
       <v-toolbar-title class="text-uppercase toolbarBM">
@@ -61,36 +16,31 @@
       </v-toolbar-items>
     </v-app-bar>
 
-    <v-sheet
-      id="scrolling-techniques-6"
-      class="overflow-y-auto"
-      max-height="600"
-    >
-      <v-container style="height: 1000px"></v-container>
-      <v-container class="heading-container">
-        <v-row class="text-center">
-          <v-col class="mb-4 headingsep">
-            <span class="display-2 font-weight-light mb-3 heading blog"
-              >Blog</span
-            >
-            <span class="display-2 font-weight-medium mb-3 heading meister"
-              >meister</span
-            >
+    <v-navigation-drawer app v-model="drawer" absolute temporary>
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+          </v-list-item-avatar>
 
-            <p class="subheading font-weight-regular firstParagraph">
-              Still in the oven!
-              <a
-                class="discord"
-                href="https://discord.gg/QFhG6jnu"
-                target="_blank"
-              >
-                Visit us on Discord!</a
-              >
-            </p>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-sheet>
+          <v-list-item-content>
+            <v-list-item-title>John Leider</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list dense>
+          <v-list-item v-for="item in items" :key="item.title" link>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
   </div>
 </template>
 
@@ -98,10 +48,8 @@
 export default {
   name: "AppBar",
   data: () => ({
-    collapseOnScroll: true,
-
     return: {
-      drawer: true,
+      drawer: false,
       items: [
         { title: "Home", icon: "mdi-view-dashboard" },
         { title: "About", icon: "mdi-forum" },
@@ -112,31 +60,4 @@ export default {
 </script>
 
 <style scoped>
-.headingsep {
-  margin-top: 12%;
-}
-
-.heading {
-  color: #212121;
-  font-size: 1.5rem;
-}
-
-.firstParagraph {
-  margin-top: 1%;
-  color: #212121;
-}
-
-.discord {
-  color: orange;
-}
-
-.blog {
-  color: orange;
-}
-
-.heading-container{
-  justify-content: center;
-  position: absolute;
-  bottom: 50%;
-}
 </style> 
